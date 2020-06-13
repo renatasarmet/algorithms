6a8b13b754f89e506c0bd66317d4eb85f2cf6fbc
Renata Sarmet <renatassmendes@hotmail.com>
2020-06-12 23:57:41 -0300

    improving day4

--- dailyProblem/day4.py
+++ dailyProblem/day4.py
@@ -1,22 +1,43 @@
 class Solution: 
+
+	def binarySearch(self, arr, start, end, target):
+		size = len(arr[start:end])
+
+		if (size == 0):
+			return (-1)
+
+		mid = start + (size // 2)
+
+		if(arr[mid] == target):
+			return (mid)
+		elif(arr[mid] > target):
+			return self.binarySearch(arr, start, mid, target)
+		else:
+			return self.binarySearch(arr, mid+1, end, target)
+
+
 	def getRange(self, arr, target):
+		# Find any occurrence
+		midTarget = self.binarySearch(arr, 0, len(arr), target)
 
-		first = -1
-		last = -1
-		qty = arr.count(target)
+		if(midTarget != -1):
+			first = midTarget
+			last = midTarget
 
-		if(qty > 0):
-			for i in range(len(arr)):
-				if arr[i] == target:
-					first = i
+			# To find the first occurrence, start from midTarget and go left
+			for i in range(midTarget-1, -1, -1):
+				if arr[i] != target:
+					first = i + 1
 					break
 
-			if(qty > 1):
-				for i in range(len(arr)-1,0,-1):
-					if arr[i] == target:
-						last = i
-						break
-			
+			# To find the last occurrence, start from midTarget and go right
+			for i in range(midTarget+1, len(arr)):
+				if arr[i] != target:
+					last = i - 1
+					break
+		else:
+			first = -1
+			last = -1
 
 		return [first,last]
     # Fill this in.
