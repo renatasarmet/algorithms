9730d65e8d8c1d6b4dedd308824df74fe37c3e10
Renata Sarmet <renatassmendes@hotmail.com>
2020-08-18 16:31:32 -0300

    Adding machine learning lalgorithms

---
+++ dailyProblem/day15.py
@@ -0,0 +1,37 @@
+# def distance(s1, s2):
+# 	# convert s1 to s2
+# 	size_s1 = len(s1)
+# 	size_s2 = len(s2)
+
+# 	d = 0
+
+# 	while i1 < size_s1 and i2 < size_s2:
+# 		if s1[i1] == s2[i2]:
+# 			i1 += 1
+# 			i2 += 1
+# 		else:
+# 			# case 1: remove letter from s1
+# 			if i1 < size_s1 - 1 and s1[i1+1] == s2[i2]:
+# 				d += 1
+# 				i1 += 1
+# 			# case 2: insert letter in s1
+# 			elif 
+
+
+
+# 	# depois tem que ver oq falta tbm da palavra maior
+
+# 	return d
+
+# print(distance('biting', 'sitting'))
+# # 2
+
+
+# biting
+# sitting
+
+# batting
+
+
+# aiii
+# iiii
